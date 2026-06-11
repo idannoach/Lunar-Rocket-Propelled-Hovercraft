0ede235a387703dd0c138e54786afb1b3648f67a
idannoach <idan.i.noach@gmail.com>
2026-06-11 14:31:04 +0300

    Clamp throttle values to the range [0, 1] to prevent negative thrust and over-unity commands

--- gnc/allocate_controls.m
+++ gnc/allocate_controls.m
@@ -13,4 +13,9 @@ function throttles = allocate_controls(desired_Fz, desired_My, B_pinv_sym)
     throttles(4) = sym_throttles_ideal(4); % Engine 4: Back
     throttles(5) = sym_throttles_ideal(3); % Engine 5: Back-Right (Ch 3)
     throttles(6) = sym_throttles_ideal(2); % Engine 6: Front-Right (Ch 2)
+
+    % Clamp to [0, 1]: prevents negative throttles (which invert thrust direction
+    % and cause mass to increase in the engine dynamics model) and over-unity
+    % commands that bypass the physical saturation logic.
+    throttles = max(0, min(1, throttles));
 end
